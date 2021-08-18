72b7de55e31c6f15e6c8a358b4ce2f1339c1f2fd
Mehmet D <exortek979@gmail.com>
2021-08-18 09:50:35 +0300

    0.0.1
    added context and Dal

---
+++ DevBackEnd.DataAccess/Concrete/EntityFramework/ETradeContext.cs
@@ -0,0 +1,25 @@
+﻿using DevBackEnd.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevBackEnd.DataAccess.Concrete.EntityFramework
+{
+    public class ETradeContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=MEMET;Database=ETRADE;Trusted_Connection=true");
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceDetail> InvoiceDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Town> Towns { get; set; }
+    }
+}
---
+++ DevBackEnd.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using DevBackEnd.Core.DataAccess.EntityFramework;
+using DevBackEnd.DataAccess.Abstract;
+using DevBackEnd.Entities.Concrete;
+
+namespace DevBackEnd.DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal:EfEntityRepositoryBase<Product,ETradeContext>,IProductDal
+    {
+        public EfProductDal(ETradeContext context) : base(context)
+        {
+        }
+    }
+}
